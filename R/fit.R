d83ca937eb85164943d55d095e48f307c7cb1846
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-02-14 16:26:35 -0800

    fit.R complete

--- R/fit.R
+++ R/fit.R
@@ -1,38 +1,31 @@
-fit = function(y, func, par1, par2, ..., sigma = 1){
+fit = function(y, func, vars, pars = {}, ..., sigma = 1){
     
-    minfunc = function(args1, args2, func, exp, sig){
+    # chi-squared function to be minimised
+    minfunc = function(argvals, argnames, extras, func, exp, sig){
         
+        # chi-squared statistic
         chisq = function(obs, exp, sig){
-            return(sum(((obs-exp)/sig)^2))
+            return(sum(((obs - exp) / sig)^2))
         }
         
-        obs = do.call(what=func, args=c(args1,args2))
+        # reconstitute args as list and run
+        args = split(argvals, argnames)
+        obs = do.call(what=func, args=c(args,extras))
+        sig = rep(sig,length(obs))[1:length(obs)]
         
+        # finish up
         return(chisq(obs=obs, exp=exp, sig=sig))
         
     }
     
+    # split vars into numeric vector and name vector (necessary due to optim requirement)
+    argvals = as.numeric(unlist(vars))
+    argnames = rep(names(vars), times=as.numeric(sapply(vars, length)))
     
-    out = nlm(f=chisq, p=as.numeric(par1), obs=do.call(what=func, args=c(par2,par1)), exp=y, sig=1)
-    
+    # run optim, reconstitute output parameters as named list
+    out = optim(par=argvals, fn=minfunc, argnames=argnames, extras=pars, func=func, exp=y, sig=sigma, ...)
+    out$par = split(as.numeric(out$par), argnames)
     return(out)
     
-}
-
-###
-
-x = 0:5
-
-y = gauss1d(x=x, fwhm=2)
-
-par1 = list(fwhm=1)
-par2 = list(x=x)
-
-func = gauss1d
-
-gauss1d = function(x, fwhm = 1, sd = fwhm / (2*(sqrt(2*log(2)))), lambda = 1){
-    mixprop = rep(lambda,length(sd))[1:length(sd)] / sum(rep(lambda,length(sd))[1:length(sd)])
-    out = sapply(X=x, FUN=dnorm, sd=sd) * mixprop
-    return(colSums(rbind(out)))
 }
 
