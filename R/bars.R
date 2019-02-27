2e032c09a7ae51eb1515b745e0a1d6096a168f43
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-02-26 16:40:10 -0800

    bars bug fix

--- R/bars.R
+++ R/bars.R
@@ -1,37 +1,47 @@
 bars = function(x, y, width, anchor = 1, joined = FALSE, col = "grey75", border = NA, ljoin = 1, ...){
     
-    # setup
-    if(missing(width) & anchor %in% c(1,3)){width = c(diff(x)[1],diff(x))}
-    if(missing(width) & anchor %in% c(2,4)){width = c(diff(y)[1],diff(y))}
-    bhw = rep(width, length(x))[1:length(x)] / 2
+    # unlog
+    xy = pos2xy(x=x, y=y)
+    opar = par()
+    if(opar$xlog){
+        xy$x = log10(xy$x)
+        par("xlog"=FALSE)
+    }
+    if(opar$ylog){
+        xy$y = log10(xy$y)
+        par("ylog"=FALSE)
+    }
+    par("usr"=opar$usr)
+    
+    # coordinates & dimensions
+    if(missing(width) & anchor %in% c(1,3)){width = c(diff(xy$x)[1],diff(xy$x))}
+    if(missing(width) & anchor %in% c(2,4)){width = c(diff(xy$y)[1],diff(xy$y))}
+    if(any(is.na(width))){width[is.na(width)] = 1}
+    bhw = rep(width, length(xy$x))[1:length(xy$x)] / 2
     base = switch(anchor, par("usr")[3], par("usr")[1], par("usr")[4], par("usr")[2])
     obase = switch(anchor, base/10, base/10, base*10, base*10)
-    if(par("ylog") & anchor %in% c(1,3)){base = 10^base}
-    if(par("xlog") & anchor %in% c(2,4)){base = 10^base}
-    if(par("ylog") & any(y<10^(par("usr")[3]))){y[y<10^(par("usr")[3])] = (10^(par("usr")[3]))/10}
-    if(par("xlog") & any(x<10^(par("usr")[1]))){x[x<10^(par("usr")[1])] = (10^(par("usr")[1]))/10}
     
     # define xy coords
     if(anchor %in% c(1,3)){
         if(joined){
-            xx = list(c(x[1]-bhw[1], x[1]-bhw[1], rep(x[-length(x)]+diff(x)/2,each=2), x[length(x)]+bhw[(length(x))], x[length(x)]+bhw[length(x)]))
-            yy = list(c(obase, rep(y,each=2), obase))
+            xx = list(c(xy$x[1]-bhw[1], xy$x[1]-bhw[1], rep(xy$x[-length(xy$x)]+diff(xy$x)/2,each=2), xy$x[length(xy$x)]+bhw[(length(xy$x))], xy$x[length(xy$x)]+bhw[length(xy$x)]))
+            yy = list(c(obase, rep(xy$y,each=2), obase))
         }else{
             xx = yy = {}
-            for(i in 1:length(x)){
-                xx = c(xx, list(c(x[i]-bhw[i], x[i]-bhw[i], x[i]+bhw[i], x[i]+bhw[i])))
-                yy = c(yy, list(c(obase, y[i], y[i], obase)))
+            for(i in 1:length(xy$x)){
+                xx = c(xx, list(c(xy$x[i]-bhw[i], xy$x[i]-bhw[i], xy$x[i]+bhw[i], xy$x[i]+bhw[i])))
+                yy = c(yy, list(c(obase, xy$y[i], xy$y[i], obase)))
             }
         }
     }else{
         if(joined){
-            xx = list(c(obase, rep(x,each=2), obase))
-            yy = list(c(y[1]-bhw[1], y[1]-bhw[1], rep(y[-length(y)]+diff(y)/2,each=2), y[length(y)]+bhw[(length(bhw))], y[length(y)]+bhw[(length(bhw))]))
+            xx = list(c(obase, rep(xy$x,each=2), obase))
+            yy = list(c(xy$y[1]-bhw[1], xy$y[1]-bhw[1], rep(xy$y[-length(xy$y)]+diff(xy$y)/2,each=2), xy$y[length(xy$y)]+bhw[(length(bhw))], xy$y[length(xy$y)]+bhw[(length(bhw))]))
         }else{
             xx = yy = {}
-            for(i in 1:length(y)){
-                xx = c(xx, list(c(obase, x[i], x[i], obase)))
-                yy = c(yy, list(c(x[i]-bhw[i], y[i]-bhw[i], y[i]+bhw[i], y[i]+bhw[i])))
+            for(i in 1:length(xy$y)){
+                xx = c(xx, list(c(obase, xy$x[i], xy$x[i], obase)))
+                yy = c(yy, list(c(xy$y[i]-bhw[i], xy$y[i]-bhw[i], xy$y[i]+bhw[i], xy$y[i]+bhw[i])))
             }
         }
     }
@@ -40,6 +50,13 @@ bars = function(x, y, width, anchor = 1, joined = FALSE, col = "grey75", border 
     for(i in 1:length(xx)){
         apolygon(x=xx[[i]], y=yy[[i]], col=col, border=border, ljoin=ljoin, ...)
     }
+    
+    # relog
+    if(opar$xlog){par("xlog"=TRUE)}
+    if(opar$ylog){par("ylog"=TRUE)}
+    par("usr"=opar$usr)
+    par("xaxp"=opar$xaxp)
+    par("yaxp"=opar$yaxp)
     
 }
 
