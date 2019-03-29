cf006865a7d4dd4d8b044cf6b681a2ff94be7a7b
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-03-29 15:48:50 -0700

    ellip2inc inc2ellip

---
+++ R/ellip2inc.R
@@ -0,0 +1,14 @@
+ellip2inc = function(x, emax = 0.8){
+    xx = pmin(pmax(x, 0), emax)
+    if(any(x < 0)){xx[x < 0] = NA}
+    if(any(x > 1)){xx[x > 1] = NA}
+    return(acos(sqrt((((1-xx)^2) - ((1-emax)^2)) / (1-((1-emax)^2)))) * (180/pi))
+}
+
+inc2ellip = function(x, emax = 0.8){
+    xx = pmin(pmax(x, 0), 90)
+    if(any(x < 0)){xx[x < 0] = NA}
+    if(any(x > 90)){xx[x > 90] = NA}
+    return(1 - sqrt(((1-((1-emax)^2))*((cos(xx*(pi/180))^2))) + ((1-emax)^2)))
+}
+
