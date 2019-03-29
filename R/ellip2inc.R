d57230f087d15b9a3887d7cce91b392abb07c75c
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-03-29 15:50:40 -0700

    ellip2inc/inc2ellip emax default update

--- R/ellip2inc.R
+++ R/ellip2inc.R
@@ -1,11 +1,11 @@
-ellip2inc = function(x, emax = 0.8){
+ellip2inc = function(x, emax = 1){
     xx = pmin(pmax(x, 0), emax)
     if(any(x < 0)){xx[x < 0] = NA}
     if(any(x > 1)){xx[x > 1] = NA}
     return(acos(sqrt((((1-xx)^2) - ((1-emax)^2)) / (1-((1-emax)^2)))) * (180/pi))
 }
 
-inc2ellip = function(x, emax = 0.8){
+inc2ellip = function(x, emax = 1){
     xx = pmin(pmax(x, 0), 90)
     if(any(x < 0)){xx[x < 0] = NA}
     if(any(x > 90)){xx[x > 90] = NA}
