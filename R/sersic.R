6f41382b45caba68e2918120a8a7d0487d03f009
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-04-05 16:54:46 -0700

    mu2r/r2mu update

--- R/sersic.R
+++ R/sersic.R
@@ -46,13 +46,33 @@ sersic.fluxfrac2r = function(fluxfrac, n = 1, r.ref = 1, fluxfrac.ref = 0.5){
     return(r)
 }
 
-sersic.r2mu = function(r, n = 1, re = 1, mu.ref = 0, r.ref = re){
+sersic.r2mu = function(r, mag = 0, n = 1, re = 1, e = 0){
+    bn = qgamma(0.5,2*n)
+    lumtot = 1*(re^2)*2*pi*n*((exp(bn))/(bn^(2*n)))*gamma(2*n)*(1-e)
+    magtot = -2.5*log10(lumtot)
+    Ie = 1/(10^(0.4*(mag-magtot)))
+    intenr = Ie*exp(-bn*(((r/re)^(1/n))-1))
+    mur = -2.5*log10(intenr)
+    return(mur)
+}
+
+sersic.mu2r = function(mu, mag = 0, n = 1, re = 1, e = 0){
+    bn = qgamma(0.5,2*n)
+    lumtot = 1*(re^2)*2*pi*n*((exp(bn))/(bn^(2*n)))*gamma(2*n)*(1-e)
+    magtot = -2.5*log10(lumtot)
+    Ie = 1/(10^(0.4*(mag-magtot)))
+    intenr = 10^(-0.4*mu)
+    rmu = re*((((log(intenr/Ie))/(-bn))+1)^n)
+    return(rmu)
+}
+
+sersic.r2mu2 = function(r, n = 1, re = 1, mu.ref = 0, r.ref = re){
     bn = qgamma(0.5,2*n)
     mu = mu.ref + ((2.5*bn)/log(10))*(((r/re)^(1/n))-((r.ref/re)^(1/n)))
     return(mu)
 }
 
-sersic.mu2r = function(mu, n = 1, re = 1, mu.ref = 0, r.ref = re){
+sersic.mu2r2 = function(mu, n = 1, re = 1, mu.ref = 0, r.ref = re){
     bn = qgamma(0.5,2*n)
     r = re * (((((log(10))*(mu-mu.ref))/(2.5*bn)) + ((r.ref/re)^(1/n)))^n)
     return(r)
